1cfb59587d1c9330720c70a3729810426e85062a
张羲 <zhangxi_ylz@newhope.cn>
2025-12-17 16:02:00 +0800

    feat: 打包配置优化

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,19 @@
 import typescript from "@rollup/plugin-typescript";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+const pkg = require("./package.json");
 
 export default {
   input: "./src/index.ts",
   output: [
     {
       format: "cjs",
-      file: "lib/min-vue.cjs.js",
+      file: pkg.main,
       sourcemap: true,
     },
     {
-      file: "lib/min-vue.esm.js",
+      file: pkg.module,
       format: "es",
       sourcemap: true,
     },
