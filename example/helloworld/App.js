c88c30637fd1edcfd2695b2a44cddfdfaef0b47d
张羲 <zhangxi_ylz@newhope.cn>
2025-12-23 15:18:44 +0800

    feat: 组件初始化渲染主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -1,9 +1,20 @@
-import { h } from "../../lib/min-vue.es.js";
+import { h } from "../../lib/mini-vue.esm.js";
 
 // 创建App组件
 export const App = {
   render() {
-    return h("div", "hi, " + this.msg);
+    return h(
+      "div",
+      {
+        id: "root",
+        class: ["red", "hard"],
+      },
+      // string
+      // "hi, " + this.msg
+
+      // array
+      [h("p", { class: "red" }, "hi"), h("p", { class: "blue" }, "vue")]
+    );
   },
 
   setup() {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared/index";
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vnode: any, container: any) {
@@ -10,13 +11,51 @@ function patch(vnode: any, container: any) {
   // 初次渲染
 
   // 判断是不是 element 类型 TODO
+  console.log(vnode.type);
 
-  processComponent(vnode, container);
+  if (typeof vnode.type === "string") {
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, container);
+  }
 }
 
 // 处理组件
 function processComponent(vnode: any, container: any) {
   mountComponent(vnode, container);
+}
+
+// 处理 element
+function processElement(vnode: any, container: any) {
+  mountElement(vnode, container);
+}
+
+function mountElement(vnode: any, container: any) {
+  const { type, children, props } = vnode;
+
+  const el = document.createElement(type);
+
+  // children -> string
+  if (typeof children === "string") {
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    mountChildren(vnode, el);
+  }
+
+  // props
+  for (let key in props) {
+    const val = props[key];
+    el.setAttribute(key, val);
+  }
+
+  container.appendChild(el);
+}
+
+// 挂载子元素
+function mountChildren(vnode: any, container: any) {
+  vnode.children.forEach((v: any) => {
+    patch(v, container);
+  });
 }
 
 // 挂载组件
